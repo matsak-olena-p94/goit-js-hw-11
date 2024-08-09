c281606afd6e9b970760e659d0b87be2f5aab29a
Olena Matsak <161434803+matsak-olena-p94@users.noreply.github.com>
2024-08-09 10:52:28 +0300

    fin

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -2,7 +2,7 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
  const gallery = document.querySelector('.gallery');
- const lightbox = new SimpleLightbox("gallery", { 
+ const lightbox = new SimpleLightbox(".gallery a" , { 
             captions: true,
             captionsData: 'alt',
             captionDelay: 250
